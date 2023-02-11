ecdcbe4ec6d1017a501eb3ad1f626f67027ce988
Mateus Falk <mateus.efalk@gmail.com>
2023-02-10 21:43:59 -0300

    DTOs

---
+++ SistemaClinicaRelatorio/src/SistemaClinicaRelatorio.Web/DTOs/MonthQueriesReportDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace SistemaClinicaRelatorio.Web.DTOs
+{
+    public class MonthQueriesReportDTO
+    {
+        [DisplayName("Código Consulta")]
+        public int id { get; set; }
+        [DisplayName("Paciente")]
+        public string pacient { get; set; }
+        [DisplayName("Médico")]
+        public string doctor { get; set; }
+        [DisplayName("Especialidade")]
+        public string specialty { get; set; }
+        [DisplayName("Data")]
+        public DateTime date { get; set; }
+        [DisplayName("Avaliação")]
+        public string assessment { get; set; }
+    }
+}
---
+++ SistemaClinicaRelatorio/src/SistemaClinicaRelatorio.Web/DTOs/PacientReportDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace SistemaClinicaRelatorio.Web.DTOs
+{
+    public class PacientReportDTO
+    {
+        [DisplayName("Código")]
+        public int id { get; set; }
+        [DisplayName("Nome")]
+        public string name { get; set; }
+        [DisplayName("Número Prontuario")]
+        public string medicalRecord { get; set; }
+        [DisplayName("CPF")]
+        public string cpf { get; set; }
+        [DisplayName("Telefone")]
+        public string fone { get; set; }
+        [DisplayName("Data de Nascimento")]
+        public string birth { get; set; }
+        [DisplayName("Estado")]
+        public string state { get; set; }
+        [DisplayName("Cidade")]
+        public string city { get; set; }
+    }
+}
