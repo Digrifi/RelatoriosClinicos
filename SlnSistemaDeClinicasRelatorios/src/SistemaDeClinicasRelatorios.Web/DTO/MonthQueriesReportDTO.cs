351d560e743711099204e7e736a29afc294e0502
Mateus Falk <mateus.efalk@gmail.com>
2023-02-14 19:10:17 -0300

    DTO, controllers, services

---
+++ SlnSistemaDeClinicasRelatorios/src/SistemaDeClinicasRelatorios.Web/DTO/MonthQueriesReportDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeClinicasRelatorios.Web.DTO
+{
+    public class MonthQueriesReport
+    {
+        [DisplayName("Código Consulta")]
+        public int id { get; set; }
+        [DisplayName("Paciente")]
+        public string pacient { get; set; }
+        [DisplayName("Médico")]
+        public string doctor { get; set; }
+        [DisplayName("Especialidade")]
+        public string specialty { get; set; }
+        [DisplayName("Data")]
+        public DateTime date { get; set; }
+        [DisplayName("Avaliação")]
+        public string assessment { get; set; }
+    }
+}
